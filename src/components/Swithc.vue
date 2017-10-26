7968dca1f6f911b723f32bef2cf6e628c5eaf91f
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2017-10-26 14:48:35 +0800

    Initialize files

---
+++ src/components/Swithc.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="on" @click='isOn = true' :class='{active: isOn}'>
+    </div>
+    <div class="off" @click='isOn = false' :class='{active: !isOn}'>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        isOn: true
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
